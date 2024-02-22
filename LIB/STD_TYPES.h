05088c6537753b4bf920b4c24b21d0f1a6b7e860
shrouk Ashraf <78410468+shroukashraf912@users.noreply.github.com>
2024-02-23 00:53:41 +0200

    Atmega32 LIB

---
+++ LIB/STD_TYPES.h
@@ -0,0 +1,32 @@
+/*
+ * STD_TYPES.h
+ *
+ *  Created on: Jul 5, 2022
+ *  Author	   : SHA
+ *  Layer      : LIB
+ *  LIB        : STD_TYPE
+ */
+
+#ifndef LIB_STD_TYPES_H_
+#define LIB_STD_TYPES_H_
+
+typedef unsigned char u8;
+
+typedef unsigned short int u16;
+typedef unsigned long  int u32;
+
+typedef signed   char s8;
+
+typedef signed   short     int s16;
+typedef signed   long      int s32;
+
+typedef float  f32;
+typedef double f64;
+
+/*
+ * Address NULL
+ */
+#define NULL ( (void *) 0x00)
+
+
+#endif /* LIB_STD_TYPES_H_ */
